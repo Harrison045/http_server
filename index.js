254a1a6556f859b8310a9d740263ea1484cbb87b
Harrison045 <harrisonprince324@gmail.com>
2023-03-30 11:55:30 +0000

    done

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const handleAllTypesOfRequest=(req,res)=>{
   res.send('welcome to home page')
 }
 
+const handleLogin=(req,res)=>{
+  res.send('this is login used with post')
+}
 //middleware function
 
 const middleWareFunction =(req,res,next)=>{
@@ -13,10 +16,17 @@ const middleWareFunction =(req,res,next)=>{
   next()
 }
 
-server.use(middleWareFunction)
+const loginRouteSpecificMiddleware=(req,res,next)=>{
+  console.log('login route has begun');
+  next()
+}
 
+//middleware
+// server.use(middleWareFunction)
+
+//routes
 server.get('/contact',(req,res)=>res.send('this is contact used with get'))
-server.post('/login',(req,res)=>res.send('this is login used with post'))
+server.post('/login',loginRouteSpecificMiddleware, handleLogin)
 server.put('/profile',(req,res)=>res.send('this is profile used with delete'))
 server.delete('/services',(req,res)=>res.send('this is services used with delete'))
 server.use('/',handleAllTypesOfRequest)
